fe036935599ea8860d0f624e2a7876cbcc8f5804
MinfredChan <204166815@qq.com>
2018-12-04 15:38:03 +0800

    ssr
     back

---
+++ ssr.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+yum install unzip wget #For Centos
+apt install unzip wget #For Debian
+cd
+wget https://www.lutizi.com/down/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
